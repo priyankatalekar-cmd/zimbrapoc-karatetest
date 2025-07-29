fac6ba9f57557ee45a38ec1d2377e024de5004e0
Priyanka Talekar <priyanka.talekar@synacor.com>
2025-07-29 17:18:35 +0530

    updated my changes

--- src/test/java/utils/DataGenerator.java
+++ src/test/java/utils/DataGenerator.java
@@ -1,4 +1,7 @@
 package utils;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.github.javafaker.Faker;
 
 public class DataGenerator {
@@ -34,5 +37,50 @@ public class DataGenerator {
     
     public static boolean getRandomBoolean() {
         return faker.bool().bool();
+    }
+    
+    public static Map<String, String> getCompleteContactData() {
+        Map<String, String> contactData = new HashMap<>();
+        
+        // Basic Info
+        contactData.put("firstName", faker.name().firstName());
+        contactData.put("lastName", faker.name().lastName());
+        contactData.put("middleName", faker.name().firstName().substring(0, 1));
+        contactData.put("email", faker.internet().emailAddress());
+        contactData.put("email2", faker.internet().emailAddress());
+        contactData.put("email3", faker.internet().emailAddress());
+        
+        // Company Info
+        contactData.put("company", faker.company().name());
+        contactData.put("jobTitle", faker.job().title());
+        
+        // Phone Numbers
+        contactData.put("workPhone", faker.phoneNumber().phoneNumber());
+        contactData.put("workPhone2", faker.phoneNumber().phoneNumber());
+        contactData.put("homePhone", faker.phoneNumber().phoneNumber());
+        contactData.put("homePhone2", faker.phoneNumber().phoneNumber());
+        contactData.put("mobilePhone", faker.phoneNumber().cellPhone());
+        contactData.put("callbackPhone", faker.phoneNumber().phoneNumber());
+        contactData.put("carPhone", faker.phoneNumber().phoneNumber());
+        contactData.put("otherPhone", faker.phoneNumber().phoneNumber());
+        contactData.put("pager", faker.number().digits(4));
+        
+        // Fax Numbers
+        contactData.put("workFax", faker.number().digits(4));
+        contactData.put("homeFax", faker.number().digits(4));
+        contactData.put("otherFax", faker.number().digits(4));
+        
+        // Work Address
+        contactData.put("workStreet", faker.address().streetAddress());
+        contactData.put("workCity", faker.address().city());
+        contactData.put("workState", faker.address().state());
+        contactData.put("workPostalCode", faker.address().zipCode());
+        contactData.put("workCountry", faker.address().country());
+        contactData.put("workURL", faker.internet().url());
+        
+        // Notes
+        contactData.put("notes", faker.lorem().paragraph());
+        
+        return contactData;
     }
 }
